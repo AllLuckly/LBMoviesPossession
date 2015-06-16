754d9a333dcbade087c6db16ebe32d4df6ae4d26
AllLuckly <435844159@qq.com>
2015-06-16 13:41:44 +0800

    Initial Commit

---
+++ LBMoviesPossession/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LBMoviesPossession
+//
+//  Created by fhkj on 15/6/16.
+//  Copyright (c) 2015年 Bison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
